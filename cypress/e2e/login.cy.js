973f722417ce2991ce6d33e3d044784211749a9e
Guilherme Silvestre <guilhermesfr268@gmail.com>
2026-08-18 21:18:13 -0300

    refactor: implementado page object para dois cenários de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,15 @@
+import LoginPage from '../page/LoginPage'
+
 describe('Realizar Login', () => {
+
+    const email = Cypress.env('USER_EMAIL')
+    const password = Cypress.env('USER_PASSWORD')
+
     context('Pagina de login disponível', () => {
         it('deve acessar o site e exibir a tela de login', () => {
             cy.paginaLogin()
         })
-})
+    })
     context('Credenciais válidas', () => {
         it('deve autenticar com credenciais válidas', () => {
             cy.paginaLogin()
@@ -12,23 +18,22 @@ describe('Realizar Login', () => {
     })
 
     context('Credenciais inválidas', () => {
-        it('deve exibir erro para credenciais inválidas', () => {
+        it('deve exibir erro para email invalido', () => {
             cy.paginaLogin()
-            cy.get('[data-testid="email-input"]').type(Cypress.env('USER_EMAIL'))
-            cy.get('[data-testid="password-input"]').type('senhaerrada')
-            cy.get('[data-testid="login-button"]').click()
-            cy.get('[data-testid="password-error"]')
-                .should('be.visible')
-                .and('contain', 'Email ou senha inválidos')
+            LoginPage.preencherEmail('teste@teste.com')
+            LoginPage.preencherSenha(password)
+            LoginPage.clickLoginButton()
+            LoginPage.messageErrorEmail()
+                .contains('Usuário não encontrado. Verifique o email ou cadastre-se.')
         })
-        it('deve exibir erro para login apenas com email', () => {
+        it.only('deve exibir erro para senha inválida', () => {
             cy.paginaLogin()
-            cy.get('[data-testid="email-input"]').type(Cypress.env('USER_EMAIL'))
-            cy.get('[data-testid="login-button"]').click()
-            cy.get('[data-testid="password-error"]')
-                .should('be.visible')
-                .and('contain', 'Email e senha são obrigatórios')
-            })
+            LoginPage.preencherEmail(email)
+            LoginPage.preencherSenha('senhaerrada')
+            LoginPage.clickLoginButton()
+            LoginPage.messageErrorSenha()
+                .contains('Email ou senha inválidos')
+        })
         it('deve exibir erro para login apenas com senha', () => {
             cy.paginaLogin()
             cy.get('[data-testid="password-input"]').type(Cypress.env('USER_PASSWORD'))
@@ -37,14 +42,14 @@ describe('Realizar Login', () => {
                 .should('be.visible')
                 .and('contain', 'Email e senha são obrigatórios')
         })
-})
+    })
 
     it('cy.login() deve funcionar via cy.session()', () => {
         cy.login()
         cy.intercept('GET', '**/products').as('getProducts')
         cy.visit('/products')
         cy.wait('@getProducts').then((interception) => {
-        expect(interception.response.statusCode).to.eq(200)
+            expect(interception.response.statusCode).to.eq(200)
         })
     })
 })
--- cypress/page/LoginPage.js
+++ cypress/page/LoginPage.js
@@ -1,4 +1,41 @@
 class LoginPage {
     // Seletores
-    
+    elements = {
+        emailInput: () => cy.get('[data-testid="email-input"]'),
+        passwordInput: () => cy.get('[data-testid="password-input"]'),
+        loginButton: () => cy.get('[data-testid="login-button"]'),
+        errorMessageSenha: () => cy.get('[data-testid="password-error"]'),
+        errorMessageEmail: () => cy.get('[data-testid="email-error"]')
+    }
+
+    preencherEmail(email){
+        this.elements.emailInput().clear().type(email)
+    }
+
+    preencherSenha(password){
+        this.elements.passwordInput().clear().type(password)
+    }
+
+    clickLoginButton(){
+        this.elements.loginButton().click()
+    }
+
+    messageErrorSenha(){
+        return this.elements.errorMessageSenha()
+        .should('be.visible')
+    }
+
+    messageErrorEmail(){
+        return this.elements.errorMessageEmail()
+        .should('be.visible')
+    }
+
+
+    login(email, senha){
+        this.preencherEmail(email)
+        this.preencherSenha(senha)
+        this.clickLoginButton()
+    }
 }
+
+export default new LoginPage()
